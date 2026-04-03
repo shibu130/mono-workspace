3166909d6429aab44687f37e882c6e666441edf6
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2026-04-04 01:18:00 +0530

    module-fed

---
+++ projects/host-app/webpack.config.js
@@ -0,0 +1,15 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  name: 'host-app',
+
+  exposes: {
+    './Component': './projects/host-app/src/app/app.component.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
