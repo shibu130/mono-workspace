6b7578161ef962d14fbfbd19318a45051933e55e
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2026-04-04 02:34:32 +0530

    refactor to non standalone ,

--- projects/host-app/src/app/app.component.ts
+++ projects/host-app/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone:false
 })
 export class AppComponent {
   title = 'host-app';
---
+++ projects/mfe-app/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule, RouterOutlet } from "@angular/router";
+import { routes } from './app.routes';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterOutlet, RouterModule.forRoot(routes)],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
