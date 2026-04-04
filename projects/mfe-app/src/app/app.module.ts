75668ef8c991e762ae7fef83bd61d6388672fa3f
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2026-04-04 14:06:00 +0530

    added routing

--- projects/mfe-app/src/app/app.module.ts
+++ projects/mfe-app/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule, RouterOutlet } from "@angular/router";
 import { routes } from './app.routes';
+import { TodoListModule } from './todo-list/todo-list.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterOutlet, RouterModule.forRoot(routes), TodoListModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ projects/mfe-app/src/app/todo-list/todo-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoListComponent } from './todo-list.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [TodoListComponent],
+  imports: [
+    CommonModule, RouterModule.forChild([{path:"", pathMatch:"full", component:TodoListComponent}])
+  ]
+})
+export class TodoListModule { }
